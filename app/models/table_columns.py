d2adda5f2e925fff8c9f5a63dd278353af56cf33
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 23:07:18 +0530

    Changes in  Choices in column types of TableColumns model

--- app/models/table_columns.py
+++ app/models/table_columns.py
@@ -12,7 +12,7 @@ CHOICES = [
 class TableColumns(models.Model):
    c_id = models.AutoField(primary_key=True)
    column_name = models.CharField(max_length=20)
-   column_type = models.CharField(CHOICES, max_length=20)
+   column_type = models.CharField(choices=CHOICES, max_length=20)
    is_primary = models.BooleanField()
    nullable = models.BooleanField(default=False)
    t_id = models.ForeignKey(TableRecords, on_delete=models.CASCADE, null=True)
