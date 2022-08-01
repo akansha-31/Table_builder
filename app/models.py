7b94c8f35fa4dd8fc64fc9af951f465a932fabd5
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 12:34:53 +0530

    Add User and TableRecords models

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
-
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+ 
+# Create your models here.
+ 
+ 
+class User(AbstractUser):
+   pass
+ 
+class TableRecords(models.Model):
+   id = models.AutoField(primary_key=True)
+   name = models.CharField(max_length=20)
+   created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+   created_at = models.DateTimeField()
