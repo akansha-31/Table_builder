7c0fbd49b3cb1093e803190acc3d5c2855e007c4
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 19:02:40 +0530

    Add url for create_table view

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
    path('', views.index, name="index"),
    path('logout/', views.logout, name="logout"),
+   path('create_table/', views.CreateTable, name="create_table")
 ]
