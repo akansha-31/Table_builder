b80aa17a07cd5fb3b168d3f03423ca4a013e3315
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 20:29:49 +0530

    Create Form instance in create_table view

--- app/views/create_table.py
+++ app/views/create_table.py
@@ -1,16 +1,21 @@
 from django.shortcuts import redirect, render
 from app.forms import TableRecordForm
+import datetime
+from app.models import TableRecords
 
 def CreateTable(request):
     if request.method == 'POST':
         form = TableRecordForm(request.POST)
         if form.is_valid():
             tablerecords = form.save(commit=False)
-            tablerecords.user = request.user
+            tablerecords.created_by = request.user
+            tablerecords.created_at = datetime.datetime.now()
             tablerecords.save()
-            return redirect('index')
+            return redirect('create_table')
     else:
         form = TableRecordForm()
+        tablerecords = TableRecords.objects.last()
 
-    context = {'form' : form}
+
+    context = {'form' : form, 'tablerecords' : tablerecords}
     return render(request, 'app/create_table.html', context)
