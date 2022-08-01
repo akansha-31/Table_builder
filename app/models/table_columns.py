41fd02e4ff65dae9c94ebf5115d4f45767b375b8
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 23:05:25 +0530

    Add Choices in column types in TableColumns model

--- app/models/table_columns.py
+++ app/models/table_columns.py
@@ -1,10 +1,18 @@
 from django.db import models
 from . import TableRecords
 
+CHOICES = [
+    ('String', 'String'),
+    ('Boolean', 'Boolean'),
+    ('Number', 'Number'),
+    ('Email', 'Email'),
+    ('DateTime', 'DateTime')
+]
+
 class TableColumns(models.Model):
    c_id = models.AutoField(primary_key=True)
    column_name = models.CharField(max_length=20)
-   column_type = models.CharField(max_length=20)
+   column_type = models.CharField(CHOICES, max_length=20)
    is_primary = models.BooleanField()
    nullable = models.BooleanField(default=False)
    t_id = models.ForeignKey(TableRecords, on_delete=models.CASCADE, null=True)
