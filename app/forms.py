99e70406050804f85084f6d3f6e2a8ee275ce1fc
Akansha Sharma <akansharma31@gmail.com>
2022-08-01 23:01:18 +0530

    Add form for columns

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm, TextInput
-from app.models import TableRecords
+from django.forms import BooleanField, ModelForm, TextInput
+from app.models import TableRecords, TableColumns
 
 
 class TableRecordForm(ModelForm):
@@ -8,4 +8,13 @@ class TableRecordForm(ModelForm):
         fields = ['name']
         widgets = {
             'name' : TextInput(attrs=({'class' : 'input', 'placeholder' : 'Enter Table name'}))
+        }
+
+class TableColumnFrom(ModelForm):
+    class Meta:
+        model = TableColumns
+        fields = ['column_name', 'column_type', 'is_primary', 'nullable']
+        widgets = {
+            'column_name' : TextInput(attrs=({'class' : 'input', 'placeholder' : 'Enter column name'})),
+            'column_type' : TextInput(attrs=({'class' : 'input', 'placeholder' : 'Enter column type'})),
         }
--- app/views/create_table.py
+++ app/views/create_table.py
@@ -1,21 +1,24 @@
 from django.shortcuts import redirect, render
-from app.forms import TableRecordForm
+from app.forms import TableRecordForm, TableColumnFrom
 import datetime
-from app.models import TableRecords
+from app.models import TableRecords, table_columns
 
 def CreateTable(request):
     if request.method == 'POST':
-        form = TableRecordForm(request.POST)
-        if form.is_valid():
+        form = TableRecordForm(request.POST or None)
+        form2 = TableColumnFrom(request.POST or None)
+        if all([form.is_valid(), form2.is_valid()]):
             tablerecords = form.save(commit=False)
+            table_columns = form2.save(commit=False)
             tablerecords.created_by = request.user
             tablerecords.created_at = datetime.datetime.now()
             tablerecords.save()
             return redirect('create_table')
     else:
         form = TableRecordForm()
+        form2 = TableColumnFrom()
         tablerecords = TableRecords.objects.last()
 
 
-    context = {'form' : form, 'tablerecords' : tablerecords}
+    context = {'form' : form, 'form2' : form2}
     return render(request, 'app/create_table.html', context)
