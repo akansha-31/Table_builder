a30128ade60a672b71ecc20d332ae5c9f3341325
Akansha Sharma <akansharma31@gmail.com>
2022-08-02 17:54:11 +0530

    Add url for create_column view

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
    path('', views.index, name="index"),
    path('logout/', views.logout, name="logout"),
-   path('create_table/', views.CreateTable, name="create_table")
+   path('create_table/', views.CreateTable, name="create_table"),
+   path('create_column/<int:columns>/', views.CreateColumn, name="create_column"),
 ]
